2cc62fac74f0577c621e2dd1e552bd78d3c4a08d
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-02 12:03:37 -0400

    feat: Cookbook.highest_calorie_meal

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,11 +1,27 @@
 class CookBook
-  attr_reader :recipes
+  attr_reader :recipes,
+              :ingredients
 
   def initialize
     @recipes = []
+    @ingredients = []
   end
 
   def add_recipe(recipe)
     @recipes << recipe
+    add_ingredient(recipe)
+  end
+
+  def add_ingredient(recipe)
+    recipe.ingredients.each do |ingredient|
+      @ingredients << ingredient.name
+    end
+    @ingredients.uniq!
+  end
+
+  def highest_calorie_meal
+    @recipes.max_by do |recipe|
+      recipe.total_calories
+    end
   end
 end
--- spec/cook_book_spec.rb
+++ spec/cook_book_spec.rb
@@ -22,8 +22,66 @@ RSpec.describe CookBook do
   end
 
   context 'Iteration 3' do
-    let(:cookbook) { CookBook.new}
+    let(:pantry) { Pantry.new }
+    let(:cookbook) { CookBook.new }
+    let(:ingredient1) { 
+      Ingredient.new(
+        {
+          name: "Cheese", 
+          unit: "C", 
+          calories: 100
+        }
+      )
+    }
+    let(:ingredient2) { 
+      Ingredient.new(
+        {
+          name: "Macaroni", 
+          unit: "oz", 
+          calories: 30
+        }
+      )
+    }
     let(:recipe1) { Recipe.new("Mac and Cheese") }
-    let(:recipe2) { Recipe.new("Cheeseburger") }
+
+    let(:ingredient3) { 
+      Ingredient.new(
+        {
+          name: "Ground Beef", 
+          unit: "oz", 
+          calories: 100
+        }
+      )
+    }
+    let(:ingredient4) { 
+      Ingredient.new(
+        {
+          name: "Bun", 
+          unit: "g", 
+          calories: 75
+        }
+      )
+    }
+    let(:recipe2) { Recipe.new("Cheese Burger")}
+
+    before(:each) do
+      recipe1.add_ingredient(ingredient1, 2)
+      recipe1.add_ingredient(ingredient2, 8)
+      recipe2.add_ingredient(ingredient1, 2)
+      recipe2.add_ingredient(ingredient3, 4)
+      recipe2.add_ingredient(ingredient4, 1)
+      cookbook.add_recipe(recipe1)
+      cookbook.add_recipe(recipe2)
+    end
+
+    it '#ingredients' do
+      expect(cookbook.ingredients).to eq(
+        ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+      )
+    end
+
+    it '#highest_calorie_meal' do
+      expect(cookbook.highest_calorie_meal).to eq(recipe2)
+    end
   end
 end
