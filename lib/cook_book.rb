a64d9a421e07d278689404c832da63bf54ca02cf
A.J. Krumholz <krumholz.alan@gmail.com>
2022-08-02 12:49:32 -0400

    feat: Cookbook.date & Cookbook.summary - complete Iteration 4

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -1,10 +1,12 @@
 class CookBook
   attr_reader :recipes,
-              :ingredients
+              :ingredients,
+              :date
 
   def initialize
     @recipes = []
     @ingredients = []
+    @date = Time.now.strftime("%m/%d/%Y")
   end
 
   def add_recipe(recipe)
@@ -22,6 +24,27 @@ class CookBook
   def highest_calorie_meal
     @recipes.max_by do |recipe|
       recipe.total_calories
+    end
+  end
+
+  def summary
+    @recipes.map do |recipe|
+      {
+        name: recipe.name,
+        details: {
+          ingredients: ingredient_detail_array(recipe),
+          total_calories: recipe.total_calories
+        }
+      }
+    end
+  end
+
+  def ingredient_detail_array(recipe)
+    recipe.ingredients_required.map do |ingredient, amount|
+      {
+        ingredient: ingredient.name, 
+        amount: "#{amount} #{ingredient.unit}"
+      }
     end
   end
 end
--- spec/cook_book_spec.rb
+++ spec/cook_book_spec.rb
@@ -84,4 +84,87 @@ RSpec.describe CookBook do
       expect(cookbook.highest_calorie_meal).to eq(recipe2)
     end
   end
+
+  context 'Iteration 4' do
+    let(:cookbook) { CookBook.new }
+
+    let(:ingredient1) { 
+      Ingredient.new(
+        {
+          name: "Cheese", 
+          unit: "C", 
+          calories: 100
+        }
+      )
+    }
+    let(:ingredient2) { 
+      Ingredient.new(
+        {
+          name: "Macaroni", 
+          unit: "oz", 
+          calories: 30
+        }
+      ) 
+    }
+    let(:recipe1) { Recipe.new("Mac and Cheese") }
+    let(:ingredient3) { 
+      Ingredient.new(
+        {
+          name: "Ground Beef", 
+          unit: "oz", 
+          calories: 100
+        }
+      )
+    }
+    let(:ingredient4) { 
+      Ingredient.new(
+        {
+          name: "Bun", 
+          unit: "g", 
+          calories: 1
+        }
+      )
+    }
+    let(:recipe2) { Recipe.new("Burger") }
+
+    before(:each) do
+      recipe1.add_ingredient(ingredient2, 8)
+      recipe1.add_ingredient(ingredient1, 2)
+      recipe2.add_ingredient(ingredient3, 4)
+      recipe2.add_ingredient(ingredient4, 100)
+      cookbook.add_recipe(recipe1)
+      cookbook.add_recipe(recipe2)
+    end
+
+    it '#date' do
+      expect(cookbook.date).to eq(
+        Time.now.strftime("%m/%d/%Y")
+      )
+    end
+
+    it '#summary' do
+      expected = [
+        {
+          :name=>"Mac and Cheese", 
+          :details=>{
+            :ingredients=>[
+              {:ingredient=>"Macaroni", :amount=>"8 oz"}, {:ingredient=>"Cheese", :amount=>"2 C"}
+              ], 
+            :total_calories=>440
+          }
+        }, 
+        {
+          :name=>"Burger", 
+          :details=>{
+            :ingredients=>[
+              {:ingredient=>"Ground Beef", :amount=>"4 oz"}, {:ingredient=>"Bun", :amount=>"100 g"}
+              ], 
+            :total_calories=>500
+          }
+        }
+      ]
+
+      expect(cookbook.summary).to eq(expected)
+    end
+  end
 end
